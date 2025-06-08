30c908875c6d1a5bd5444663b6fa40e1d1f790b2
Calebe <calebepfinger@gmail.com>
2025-06-08 13:26:56 -0300

    Implementação do CRUD no ModuloRevistas

--- ClubeDaLeitura.ConsoleApp/ModuloRevistas/Revistas.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloRevistas/Revistas.cs
@@ -1,6 +1,44 @@
-﻿namespace ClubeDaLeitura.ConsoleApp.Modulo_de_Revistas
+﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
+using ClubeDaLeitura.ConsoleApp.Modulo_de_Caixas;
+
+namespace ClubeDaLeitura.ConsoleApp.Modulo_de_Revistas
 {
-    public class Revista
+    public class Revista : EntidadeBase
     {
+        public Revista(string titulo, int numeroEdicao, DateTime anoPublicacao, Caixa caixa)
+        {
+            Titulo = titulo;
+            NumeroEdicao = numeroEdicao;
+            AnoPublicacao = anoPublicacao;
+            Caixa = caixa;
+        }
+
+        public string Titulo { get; set; }
+        public int NumeroEdicao { get; set;}
+        public DateTime AnoPublicacao { get; set;}
+        public Caixa Caixa { get; set;}
+
+        public override void AtualizarRegistro(EntidadeBase registroAtualizado)
+        {
+            Revista revista = (Revista)registroAtualizado;
+
+            Titulo = revista.Titulo;
+            NumeroEdicao = revista.NumeroEdicao;
+            AnoPublicacao = revista.AnoPublicacao;
+            Caixa = revista.Caixa;  
+        }
+
+        public override string Validar()
+        {
+            string erros = string.Empty;
+
+            if (Titulo.Length < 2 || Titulo.Length > 100)
+                erros += "O campo Título deve conter entre 2 e 100 caracteres";
+
+            if (NumeroEdicao < 0)
+                erros += "O campo Número de Edição deve ser positívo";
+
+            return erros;
+        }
     }
 }
--- ClubeDaLeitura.ConsoleApp/ModuloRevistas/TelaRevista.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloRevistas/TelaRevista.cs
@@ -1,6 +1,61 @@
-﻿namespace ClubeDaLeitura.ConsoleApp.Modulo_de_Revistas
+﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
+using ClubeDaLeitura.ConsoleApp.Modulo_de_Amigos;
+using ClubeDaLeitura.ConsoleApp.Modulo_de_Caixas;
+
+namespace ClubeDaLeitura.ConsoleApp.Modulo_de_Revistas
 {
-    public class TelaRevista
+    public class TelaRevista : TelaBase
     {
+        public TelaRevista(RepositorioRevista repositorio) : base("Revista", repositorio)
+        {
+        }
+
+        public override void VisualizarRegistros()
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Visualização de Revistas");
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine(
+                "{0, -6} | {1, -30} | {2, -30} | {3, -20} | {4, -20} | {5, -10}",
+                "Id", "Título", "Número de Edição", "Ano de Publicação", "Caixa", "Status"
+            );
+
+            EntidadeBase[] revistas = repositorio.SelecionarRegistros();
+
+            for (int i = 0; i < revistas.Length; i++)
+            {
+                Revista r = (Revista)revistas[i];
+
+                if (r == null)
+                    continue;
+
+                Console.WriteLine(
+                    "{0, -6} | {1, -30} | {2, -30} | {3, -20} | {4, -20} | {5, -10}",
+                    r.Id, r.Titulo, r.NumeroEdicao, r.AnoPublicacao, r.Caixa
+                );
+            }
+            Console.ReadLine();
+        }
+
+        protected override EntidadeBase ObterDados()
+        {
+            Console.WriteLine("Qual é o Título da Revista: ");
+            string titulo = Console.ReadLine();
+
+            Console.WriteLine("Qual é Número da Edição dessa Revista: ");
+            int numeroEdicao = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual o ano de Publicação: ");
+            DateTime anoPublicacao = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("Qual o ID da caixa da revista: ");
+            Caixa caixa = ;
+
+            Revista revista = new(titulo, numeroEdicao, anoPublicacao, caixa);
+
+            return revista;
+        }
     }
 }
